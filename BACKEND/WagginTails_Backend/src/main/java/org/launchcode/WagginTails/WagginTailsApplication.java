8cc31125dc1a3746e4a91352fb9bad30ce76e8a3
snehachitre-apps <sneha.chitre.usa@gmail.com>
2023-12-05 19:27:21 -0600

    folder structure done

---
+++ BACKEND/WagginTails_Backend/src/main/java/org/launchcode/WagginTails/WagginTailsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.WagginTails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagginTailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WagginTailsApplication.class, args);
+	}
+
+}
---
+++ BACKEND/WagginTails_Backend/src/test/java/org/launchcode/WagginTails/WagginTailsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.WagginTails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagginTailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
