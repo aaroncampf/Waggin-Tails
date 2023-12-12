e9180c69c24ac5cd729a6a186e5356779231ce73
Bridget <bridget.carlile@gmail.com>
2023-12-11 20:19:05 -0600

    bare bones of homepage

---
+++ FRONTEND/WagginTails_Frontend/src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function HeroSection() {
+    return(
+    <div className='HeroSection'>
+        <h1>Ready to meet your new best friend?</h1>
+        <img src={'/src/images/HeroImage.jpeg'} width={1200}></img>
+    </div>
+    )
+}
+
+export default HeroSection
