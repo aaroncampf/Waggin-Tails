2e51bf69d76b2576a147543d158ee3469b440f31
kyariaclemons <kyarialove@gmail.com>
2023-12-12 00:15:45 -0600

    Added packages under main java: controllers, models(Data-repository layer). added fields getters and settters for userinformation model.

---
+++ BACKEND/WagginTails_Backend/src/main/java/org/launchcode/WagginTails/models/userInformation.java
@@ -0,0 +1,43 @@
+package org.launchcode.WagginTails.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class userInformation {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String userName;
+    private String password;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
