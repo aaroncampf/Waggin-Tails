3d3bf0e475fed753346d7403cd1abda7d8e1cb5c
Jenna Tan <9787333+jenna215@users.noreply.github.com>
2024-01-15 12:17:10 -0500

    submission message ui changes

--- FRONTEND/WagginTails_Frontend/src/components/SubmissionConfirmation.jsx
+++ FRONTEND/WagginTails_Frontend/src/components/SubmissionConfirmation.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Steps.css';
+
 const SubmissionConfirmation = () => {
   return (
-    <div>
+    <div className="step-container">
       <h2>Application Complete!</h2>
       <p>Your adoption application has been successfully submitted.</p>
       <p>You can expect a response in 1-2 days. Please find more details about us here:  <Link to="/about">HERE</Link></p>
